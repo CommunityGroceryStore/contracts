4df24d700aac9ef3a0a952b5126696558c9728c4
Jim Toth <jim@artby.city>
2025-05-10 12:28:08 -0400

    Remove unused boilerplate files. Adds fockerfile & github build workflow

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:22-slim
+WORKDIR /usr/src/app
+COPY package.json package-lock.json ./
+RUN npm install
+COPY . .
+RUN npx hardhat compile
--- ignition/modules/CGSToken.ts
+++ ignition/modules/CGSToken.ts
@@ -1,10 +1,13 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+import { ethers } from 'hardhat'
 
 const CGSTokenModule = buildModule('CGSTokenModule', m => {
   const ownerAddress = m.getParameter('ownerAddress')
-  const initialSupply = m.getParameter('initialSupply', BigInt(1_000_000_000) * BigInt(1e18))
-
-  const token = m.contract('CGSToken', [ownerAddress, initialSupply])
+  const initialSupply = m.getParameter(
+    'initialSupply',
+    ethers.parseUnits('1000000000', 18)
+  )
+  const token = m.contract('CGSToken', [ ownerAddress, initialSupply ])
 
   return { token }
 })
